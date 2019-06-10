3ab857b104de6292f77d1ffa421118c37115fe9f
iMakca <ukr-maksim1@yandex.ru>
2019-06-10 21:32:27 +0300

    added project

---
+++ SwiftUI-Facebook/SwiftUI-Facebook/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI-Facebook
+//
+//  Created by Maxim Granchenko on 6/10/19.
+//  Copyright © 2019 Maxim Granchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        NavigationView {
+//            navigationBarTitle(Text("Group"))
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
