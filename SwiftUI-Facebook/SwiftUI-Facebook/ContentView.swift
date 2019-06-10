09f653499aa19209bddc40697cac25ee6f125d50
iMakca <ukr-maksim1@yandex.ru>
2019-06-10 21:53:51 +0300

    added navigationTitle, List

--- SwiftUI-Facebook/SwiftUI-Facebook/ContentView.swift
+++ SwiftUI-Facebook/SwiftUI-Facebook/ContentView.swift
@@ -9,10 +9,39 @@
 import SwiftUI
 
 struct ContentView : View {
+    
+    private let posts = ["1", "2", "3", "4", "5", "6"]
+    
     var body: some View {
         NavigationView {
-//            navigationBarTitle(Text("Group"))
+            List {
+                ScrollView {
+                    VStack (alignment: .leading) {
+                        Text("Trending")
+                            .font(.subheadline)
+                        HStack {
+                            Text("Scroll - 1")
+                            Text("Scroll - 1")
+                            Text("Scroll - 1")
+                            Text("Scroll - 1")
+                            Text("Scroll - 1")
+                            Text("Scroll - 1")
+                            Text("Scroll - 1")
+                            Text("Scroll - 1")
+                        }
+                    }
+                    }.frame(height: 150)
+                ForEach(posts.identified(by: \.self)) { post in
+                    PostView()
+                }
+                }.navigationBarTitle(Text("Group"))
         }
+    }
+}
+
+struct PostView: View {
+    var body: some View {
+        Text("Hi every body")
     }
 }
 
